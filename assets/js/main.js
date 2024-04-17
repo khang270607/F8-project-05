4f1c4715e247530242da27f6660fbd17e075fb20
Huỳnh Vũ Khang <146689046+khang270607@users.noreply.github.com>
2024-04-17 17:39:52 +0700

    fix input email

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,24 +62,40 @@
 $("#button1").click(function () {
     let email1 = $("#email1").val();
     let modal = $("#modal").hide();
-
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailMinLength = 11;
+    var specialChars = /[!#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/;
     $(".closeModal").click(function () {
         $(modal).hide();
     });
 
-    if (email1.indexOf("@gmail.com") !== -1) {
+    if (email1.indexOf("@gmail.com") !== -1 && !specialChars.test(email1)) {
+        if (email1.lenght >= emailMinLength && emailRegex.test(email1)) {
+            $(modal).show();
+            $(".modal__title").text("Đăng nhập thất bại ");
+            $(".modal__body").text(
+                "Bạn cần nhập đúng định dạng email, phải có từ 11 kí tự trở lên và không chứa kí tự đặt biệt"
+            );
+            $(".modal__showEmail").text("vd: abc@gmail.com");
+            $(".modal__showEmail").css("color", "#000");
+            $(".closeModal").click(function () {
+                $("#email1").focus();
+            });
+        }
         $(modal).show();
         $(".modal__title").text("Đăng nhập thành công");
         $(".modal__body").text("Bạn đang đăng nhập với email là : ");
         $(".modal__showEmail").css("color", "#1106af");
         $(".modal__showEmail").text(email1);
         $(".closeModal").click(function () {
-            $("#email1").val("");
+            $("#email1").val("").blur();
         });
     } else {
         $(modal).show();
         $(".modal__title").text("Đăng nhập thất bại ");
-        $(".modal__body").text("Bạn cần nhập đúng định dạng email ");
+        $(".modal__body").text(
+            "Bạn cần nhập đúng định dạng email, phải có từ 11 kí tự trở lên và không chứa kí tự đặt biệt"
+        );
         $(".modal__showEmail").text("vd: abc@gmail.com");
         $(".modal__showEmail").css("color", "#000");
         $(".closeModal").click(function () {
@@ -91,12 +107,26 @@ $("#button1").click(function () {
 $("#button2").click(function () {
     let email2 = $("#email2").val();
     let modal = $("#modal").hide();
-
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailMinLength = 11;
+    var specialChars = /[!#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/;
     $(".closeModal").click(function () {
-            $(modal).hide();
+        $(modal).hide();
     });
 
-    if (email2.indexOf("@gmail.com") !== -1) {
+    if (email2.indexOf("@gmail.com") !== -1 && !specialChars.test(email2)) {
+        if (email2.lenght >= emailMinLength && emailRegex.test(email2)) {
+            $(modal).show();
+            $(".modal__title").text("Đăng nhập thất bại ");
+            $(".modal__body").text(
+                "Bạn cần nhập đúng định dạng email, phải có từ 11 kí tự trở lên và không chứa kí tự đặt biệt"
+            );
+            $(".modal__showEmail").text("vd: abc@gmail.com");
+            $(".modal__showEmail").css("color", "#000");
+            $(".closeModal").click(function () {
+                $("#email2").focus().val(email2);
+            });
+        }
         $(modal).show();
         $(".modal__title").text("Đăng kí thành công");
         $(".modal__body").text("Bạn đang đăng nhập với email là : ");
@@ -108,11 +138,13 @@ $("#button2").click(function () {
     } else {
         $(modal).show();
         $(".modal__title").text("Đăng nhập thất bại ");
-        $(".modal__body").text("Bạn cần nhập đúng định dạng email ");
+        $(".modal__body").text(
+            "Bạn cần nhập đúng định dạng email, phải có từ 11 kí tự trở lên và không chứa kí tự đặt biệt"
+        );
         $(".modal__showEmail").text("vd: abc@gmail.com");
         $(".modal__showEmail").css("color", "#000");
         $(".closeModal").click(function () {
-            $("#email2").focus();
+            $("#email2").focus().val(email2);
         });
     }
 });
